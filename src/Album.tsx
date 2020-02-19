5fb1382eb4d8de463552e7a1c93c8f9bebc578e9
Justin Maple <justinamaple@gmail.com>
2020-02-18 23:07:25 -0800

    Pulled out Footer into it's own Component

--- src/Album.tsx
+++ src/Album.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import NavBar from './NavBar';
-import Copyright from './Copyright';
 import HeroContent from './HeroContent';
 import CardContainer from './CardContainer';
-
-const useStyles = makeStyles(theme => ({
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
+import Footer from './Footer';
 
 export default function Album() {
-  const classes = useStyles();
-
   return (
     <>
       <CssBaseline />
@@ -25,15 +14,7 @@ export default function Album() {
         <HeroContent />
         <CardContainer />
       </main>
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
+      <Footer />
     </>
   );
 }
