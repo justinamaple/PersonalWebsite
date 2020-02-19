c6d51645d4dce2276312239ac37d60d5942297a0
Justin Maple <justinamaple@gmail.com>
2020-02-18 19:47:43 -0800

    Added Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Album from './Album';
 
 function App() {
   function greeter(person: string) {
@@ -12,18 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Or Don't lol.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Album />
       </header>
     </div>
   );
