ffb8326e9d2d07c82632d9163404965c0cf4593d
Justin Maple <justinamaple@gmail.com>
2020-02-18 23:04:18 -0800

    Pulled out CardContainer into it's own Component

--- src/Album.tsx
+++ src/Album.tsx
@@ -1,48 +1,18 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import NavBar from './NavBar';
 import Copyright from './Copyright';
 import HeroContent from './HeroContent';
+import CardContainer from './CardContainer';
 
 const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
   footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
 }));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Album() {
   const classes = useStyles();
@@ -52,44 +22,9 @@ export default function Album() {
       <CssBaseline />
       <NavBar />
       <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <HeroContent />
-        </div>
-        {/* End hero unit */}
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map(card => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+        <HeroContent />
+        <CardContainer />
       </main>
-      {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
           Footer
@@ -99,7 +34,6 @@ export default function Album() {
         </Typography>
         <Copyright />
       </footer>
-      {/* End footer */}
     </>
   );
 }
--- src/HeroContent.tsx
+++ src/HeroContent.tsx
@@ -3,8 +3,13 @@ import Typography from '@material-ui/core/Typography';
 import { Container, Grid, Button, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
   heroButtons: {
     marginTop: theme.spacing(4),
+  
   },
 }));
 
@@ -12,29 +17,31 @@ export default function HeroContent() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="sm">
-      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-        Album layout
-      </Typography>
-      <Typography variant="h5" align="center" color="textSecondary" paragraph>
-        Something short and leading about the collection below—its contents, the creator, etc.
-        Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-        entirely.
-      </Typography>
-      <div className={classes.heroButtons}>
-        <Grid container spacing={2} justify="center">
-          <Grid item>
-            <Button variant="contained" color="primary">
-              Main call to action
-            </Button>
+    <div className={classes.heroContent}>
+      <Container maxWidth="sm">
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          Album layout
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          Something short and leading about the collection below—its contents, the creator, etc.
+          Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+          entirely.
+        </Typography>
+        <div className={classes.heroButtons}>
+          <Grid container spacing={2} justify="center">
+            <Grid item>
+              <Button variant="contained" color="primary">
+                Main call to action
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary">
+                Secondary action
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button variant="outlined" color="primary">
-              Secondary action
-            </Button>
-          </Grid>
-        </Grid>
-      </div>
-    </Container>
+        </div>
+      </Container>
+    </div>
   );
 }
